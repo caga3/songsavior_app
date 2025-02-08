b20e6b80f39eab6842b1a7e2c62b91ad28d6997d
Alex Nguyen <alex@petrolad.com>
2025-02-07 20:17:46 -0800

    1

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import { enableScreens } from 'react-native-screens';
+import {enableScreens} from 'react-native-screens';
 import {AuthProvider} from './src/context/AuthContext';
 import AppNav from './src/navigation/AppNav';
 import DeviceInfo from 'react-native-device-info';
@@ -10,26 +10,15 @@ enableScreens();
 function App() {
   useEffect(() => {
     const checkForUpdate = async () => {
-      const currentVersion = await DeviceInfo.getVersion();
-
-      checkIfKeyExists('appVersion').then((exists) => {
-  console.log(exists ? 'Key exists!' : 'Key does not exist.');
-});
-      try {
-        const storedVersion = await AsyncStorrage.getItem('appVersion');
-        console.log(currentVersion);
-        console.log(storedVersion);
-
-        if (storedVersion !== currentVersion) {
-          //await AsyncStorage.setItem('appVersion', currentVersion);
-          //await AsyncStorage.clear(); // Clear storage
-        }
-      } catch(error) {
-        console.error(123);
-        return null;
+      const currentVersion = DeviceInfo.getVersion();
+      const storedVersion = await AsyncStorage.getItem('appVersion');
+      console.log(currentVersion);
+      if (storedVersion !== currentVersion) {
+        await AsyncStorage.setItem('appVersion', currentVersion);
+        await AsyncStorage.clear();
       }
     };
-
+    // Check for Updates to Clear Data
     checkForUpdate();
   }, []);
   return (
